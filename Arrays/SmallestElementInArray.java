8c71ddffd89a8722afb12ef3a76a09f6d11a4ce6
abavadha <abavadha@ford.com>
2024-05-17 22:58:04 +0530

    Smallest element and Prime Number programs

---
+++ Arrays/SmallestElementInArray.java
@@ -0,0 +1,31 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SmallestElementInArray {
+    public static int findMin(int[] arr) {
+        int smallest = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i]<smallest) {
+                smallest=arr[i];
+            }
+        }
+        return smallest;
+    }
+
+    public static void main(String[] args){
+        int[] arr = new int[6];
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the array elements: ");
+        for(int i=0;i<arr.length;i++){
+            System.out.println("Enter the element "+i+": ");
+            arr[i] = input.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+        int min = findMin(arr);
+        System.out.println("Minimum number is: "+min);
+
+    }
+}
+
---
+++ BasicJava/Prime.java
@@ -0,0 +1,32 @@
+package BasicJava;
+
+import java.util.Scanner;
+
+public class Prime {
+    public static boolean isNumberPrime(int number){
+        for(int i=2;i<=number/2;i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+            return true;
+    }
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = input.nextInt();
+        if(number<=1){
+            System.out.println("Not Prime");
+            return;
+        }
+        boolean isPrime = isNumberPrime(number);
+
+        // print result based on whether the number is prime or not
+        if (isPrime) {
+            System.out.println("Prime");
+        } else {
+            System.out.println("Not Prime");
+        }
+
+    }
+}
