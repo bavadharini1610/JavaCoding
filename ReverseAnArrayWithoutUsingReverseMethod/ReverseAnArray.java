0e465524fc90c1fe6408e203a3f25785248f221b
bavadharini <bavadharini.a@gmail.com>
2024-06-11 10:27:13 +0530

    Reverse an array without reverse method

--- ReverseAnArrayWithoutUsingReverseMethod/ReverseAnArray.java
+++ ReverseAnArrayWithoutUsingReverseMethod/ReverseAnArray.java
@@ -1,2 +1,29 @@
-package ReverseAnArrayWithoutUsingReverseMethod;public class ReverseAnArray {
+package ReverseAnArrayWithoutUsingReverseMethod;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReverseAnArray {
+    public static int[] reverseArray(int[] arr){
+        int[] reverse = new int[arr.length];
+        int j=0;
+        for(int i=arr.length-1;i>=0;i--){
+            reverse[j]=arr[i];
+            j++;
+        }
+        return reverse;
+    }
+    public static void main(String[] args) {
+        int[] arr;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the length of the Array: ");
+        int length = input.nextInt();
+        arr=new int[length];
+        for(int i=0;i<length;i++){
+            System.out.println("Enter the array element "+i+" :");
+            arr[i]=input.nextInt();
+        }
+        int[] reverse = reverseArray(arr);
+        System.out.println(Arrays.toString(reverse));
+    }
 }
