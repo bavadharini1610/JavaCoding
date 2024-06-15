0d0fb01f3e9af1cc024592dc3c8b1628bcea99db
bavadharini <bavadharini.a@gmail.com>
2024-06-15 09:10:38 +0530

    Amstrong Number

---
+++ AmstrongNumber/AmstrongNumber.java
@@ -0,0 +1,29 @@
+package AmstrongNumber;
+
+import java.util.Scanner;
+
+public class AmstrongNumber {
+    public static void checkAmstrong(int number){
+        String num=Integer.toString(number);
+        int length = num.length();
+        char[] nums = num.toCharArray();
+        int sum=0;
+        for(int i=0;i<num.length();i++){
+            int a = nums[i]-'0';
+            sum= (int) (sum+Math.pow(a,length));
+        }
+        if(sum==number){
+            System.out.println("Amstrong");
+        }
+        else{
+            System.out.println("Not an Amstrong");
+        }
+    }
+    public static void main(String[] args) {
+        int number;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the Number: ");
+        number=input.nextInt();
+        checkAmstrong(number);
+    }
+}
